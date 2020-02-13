43ce6e9e73c44ad129f4b08b589b0cdcf0900829
AudreyHorne <audreyhorne@mymail.mines.edu>
2020-02-13 12:37:20 -0700

    Initial commit for exceptions project.

---
+++ src/NegativeBalanceException.java
@@ -0,0 +1,9 @@
+
+public class NegativeBalanceException extends Exception {
+	private double balance;
+
+	public NegativeBalanceException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
